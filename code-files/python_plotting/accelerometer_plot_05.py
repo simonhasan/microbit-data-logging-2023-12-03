02bb0186a55152cd7b187429de0ae03eaadce8c8
Simon <simon@hasanprep.org>
2023-12-01 23:06:31 -0700

    Update accelerometer_plot_05.py
    Added more comments.

--- code-files/python_plotting/accelerometer_plot_05.py
+++ code-files/python_plotting/accelerometer_plot_05.py
@@ -13,9 +13,14 @@ accelerometer_df = pd.read_csv('data/accelerometer.csv', # read the CSV file as 
 fig, axes = plt.subplots(nrows=3) # nrows=3 for 3 rows of plots
 
 # Slice the data into x, y, and z columns
-accelerometer_df['x'].plot(ax=axes[0]) # x in blue (default)
-accelerometer_df['y'].plot(ax=axes[1], color='red') # y in red
-accelerometer_df['z'].plot(ax=axes[2], color='green') # z in green
+# [x] in square brackets to slice the x column
+accelerometer_df['x'].plot(ax=axes[0]) # ax=axes[0] to plot on the first plot, x in blue (default)
+# [y] in square brackets to slice the y column
+accelerometer_df['y'].plot(ax=axes[1], # ax=axes[1] to plot on the second plot
+                           color='red') # y in red
+# [z] in square brackets to slice the z column
+accelerometer_df['z'].plot(ax=axes[2], # ax=axes[2] to plot on the third plot
+                           color='green') # z in green
 
 # Add a title to the figure
 fig.suptitle('Accelerometer Data')
